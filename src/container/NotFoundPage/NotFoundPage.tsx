a1bf41de6f835bd6efb1ae3402971900aed4e82b
Jedrzej <progreacja@gmail.com>
2020-09-21 07:20:32 +0200

    Adjust Image

--- src/container/NotFoundPage/NotFoundPage.tsx
+++ src/container/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'gatsby'
 import React from 'react'
-import {FaArrowLeft, FaTimesCircle} from 'react-icons/all'
-import { Markdown } from '../../components/Markdown/Markdown'
+import { FaArrowLeft, FaTimesCircle } from 'react-icons/all'
 import { ResponsiveImg } from '../../components/ResponsiveImg/ResponsiveImg'
 import { NotFoundDataQuery } from '../../graphql-types'
 import * as styles from './NotFoundPage.module.scss'
@@ -13,8 +12,13 @@ interface Props {
 export const NotFoundPage: React.FC<Props> = ({ data }) => {
   return (
     <div className={styles.wrapper}>
-      <ResponsiveImg image={data.notfound.picture} alt={'banner'} className={styles.banner} />
-      <FaTimesCircle className={styles.icon}/>
+      <ResponsiveImg
+        image={data.notfound.picture}
+        alt={'banner'}
+        className={styles.banner}
+        imgStyle={{objectPosition: '50% 40%'}}
+      />
+      <FaTimesCircle className={styles.icon} />
       <p>{data.notfound.text}</p>
       <Link to="/" className={styles.link}>
         <FaArrowLeft />
