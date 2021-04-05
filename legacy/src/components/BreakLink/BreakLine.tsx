3f3571e7d6ebc49b3f00fbcc258907fa995354f0
Jedrzej <progreacja@gmail.com>
2021-04-05 16:23:17 +0200

    Cleanup

--- legacy/src/components/BreakLink/BreakLine.tsx
+++
@@ -1,8 +0,0 @@
-import React, { forwardRef } from 'react';
-import * as styles from './BreakLine.module.scss';
-
-export const BreakLine = forwardRef<HTMLDivElement>((props, ref) => {
-  return <div className={styles.breakline} ref={ref} />;
-});
-
-BreakLine.displayName = 'BreakLine';
