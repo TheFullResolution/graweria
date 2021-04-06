05184d2dd00789106f5367ffc0ec1455e70cda11
Jedrzej <progreacja@gmail.com>
2021-04-06 08:14:36 +0200

    Fixes Trt 3

--- src/api/blogList/checkIfMetaWorks.ts
+++
@@ -1,5 +0,0 @@
-import type { Meta } from '../../types/blogList';
-
-export function checkIfMetaWorks(data: unknown): data is Meta {
-  return typeof data === 'object' && (data as Meta)?.title !== undefined;
-}
