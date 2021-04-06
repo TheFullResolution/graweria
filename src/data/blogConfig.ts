62d1b8ba940c873ceb2be83c2a3bd3bc6093d4f2
Jedrzej <progreacja@gmail.com>
2021-04-06 06:39:31 +0200

    CMS changes

--- src/data/blogConfig.ts
+++ src/data/blogConfig.ts
@@ -1,1 +1,9 @@
-export const BLOG_PATH = './src/cms/blog';
+import path from 'path';
+
+const BLOG_PATH = 'public/cms/blog';
+
+export const getBlogPath = () => {
+  const projectDir = process.cwd();
+
+  return path.join(projectDir, BLOG_PATH);
+};
