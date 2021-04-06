29cf53843f857623dd9147a386e541a4dbfe95d4
Jedrzej Lewandowski <progreacja@gmail.com>
2021-04-06 07:10:00 +0200

    test 2 (#12)

--- src/data/blogConfig.ts
+++ src/data/blogConfig.ts
@@ -1,9 +1,5 @@
-import path from 'path';
-
-const BLOG_PATH = 'public/cms/blog';
+const BLOG_PATH = './public/cms/blog';
 
 export const getBlogPath = () => {
-  const projectDir = process.cwd();
-
-  return path.join(projectDir, BLOG_PATH);
+  return BLOG_PATH;
 };
