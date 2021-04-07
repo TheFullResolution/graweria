d9f717ce71a40eb11329af82026bb047b43f91f8
Jedrzej <progreacja@gmail.com>
2021-04-07 06:23:02 +0200

    Add Widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import 'focus-visible/dist/focus-visible.min.js';
 import type { AppProps as NextAppProps } from 'next/app';
 import '../styles/global.scss';
+import { useAsync } from 'react-use';
 
 function MyApp({ Component, pageProps }: NextAppProps) {
+  useAsync(async () => {
+    await import('netlify-identity-widget');
+  });
   return <Component {...pageProps} />;
 }
 
