b5480d0e5eb43851b9fdd89e9ade43e891a8625c
TheFullResolution <progreacja@gmail.com>
2021-05-06 16:33:39 +0200

    Cleanup

--- next.config.js
+++ next.config.js
@@ -1,41 +1,36 @@
 /* eslint-disable @typescript-eslint/no-var-requires,prettier/prettier,import/order */
 const path = require('path');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
-module.exports =
-  withBundleAnalyzer({
-    target: "serverless",
-    future: {
-      webpack5: true,
-    },
-    sassOptions: {
-      //LOOK OUT FOR THAT SHIT - CHANGES NAMES DEPENDS ON RELEASE (NEXTJS uses prependData, sass loader uses additionalData)
-      prependData: '@import "src/styles";',
-      includePaths: [path.join(__dirname, 'src/styles')],
-    },
-    async headers() {
-      return [
-        {
-          source: '/:all*(svg|jpg|png|JPG)',
-          locale: false,
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public, max-age=31540000, must-revalidate',
-            }
-          ],
-        },
-      ]
-    },
-    webpack: (config, { dev, isServer }) => {
+module.exports = {
+  target: 'serverless',
+  future: {
+    webpack5: true,
+  },
+  sassOptions: {
+    //LOOK OUT FOR THAT SHIT - CHANGES NAMES DEPENDS ON RELEASE (NEXTJS uses prependData, sass loader uses additionalData)
+    prependData: '@import "src/styles";',
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|JPG)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31540000, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (dev && isServer) {
+      const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
 
-      if (dev && isServer) {
-        const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-        config.plugins.push(new ForkTsCheckerWebpackPlugin());
-      }
-
-      return config;
-    },
-  });
+    return config;
+  },
+};
