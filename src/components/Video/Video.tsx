06e1018fa5f8b56b0528772a9f035917f9a0c5de
TheFullResolution <progreacja@gmail.com>
2021-05-25 07:42:15 +0200

    Super Lazy Load Youtube

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,5 +1,8 @@
 import cls from 'clsx';
+import Image from 'next/image';
 import React from 'react';
+import { useBoolean, useEffectOnce } from 'react-use';
+import { isBrowser } from 'react-use/lib/misc/util';
 import styles from './Video.module.scss';
 
 interface Props {
@@ -13,16 +16,48 @@ export const Video: React.FC<Props> = ({
   videoTitle,
   className,
 }) => {
+  const [showIframe, toggleShowIframe] = useBoolean(false);
+
+  const setShowIframeTrue = () => {
+    if (!showIframe) {
+      toggleShowIframe(true);
+    }
+  };
+
+  useEffectOnce(() => {
+    const initIframe = (event: Event) => {
+      setShowIframeTrue();
+      event.currentTarget?.removeEventListener(event.type, initIframe); // remove the event listener that got triggered
+    };
+    if (isBrowser) {
+      document.addEventListener('scroll', initIframe);
+      document.addEventListener('mousemove', initIframe);
+      document.addEventListener('touchstart', initIframe);
+      document.addEventListener('DOMContentLoaded', () => {
+        setTimeout(setShowIframeTrue, 3500);
+      });
+    }
+  });
   return (
     <div className={cls(styles.video, className)}>
-      <iframe
-        src={videoSrcURL}
-        title={videoTitle}
-        loading="lazy"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        frameBorder="0"
-        allowFullScreen
-      />
+      {showIframe ? (
+        <iframe
+          src={videoSrcURL}
+          title={videoTitle}
+          loading="lazy"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          allowFullScreen
+        />
+      ) : (
+        <Image
+          src="/images/youtube.png"
+          className={styles.image}
+          alt={'Screenshot z Youtube'}
+          width={490}
+          height={290}
+        />
+      )}
     </div>
   );
 };
